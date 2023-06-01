cd4c28712a7f774a294e1098236c169426501f78
user404e <e404user@gmail.com>
2023-06-01 18:57:32 +0000

    PHP

---
+++ 5.2/MatrixMultiplication.php
@@ -0,0 +1,33 @@
+<?php
+    $m1 = array(
+        array(1,2,3),
+        array(1,2,3),
+        array(1,2,3)
+    );
+
+    $m2 = array(
+        array(1,2,3),
+        array(1,2,3),
+        array(1,2,3)
+    );
+
+    $ans = array(
+        array(1,2,3),
+        array(1,2,3),
+        array(1,2,3)
+    );
+    for($i=0;$i<3;$i++){
+        for($j=0;$j<3;$j++){
+            for($k=0;$k<3;$k++){
+                $ans[$i][$j] += $m1[$i][$k] * $m2[$k][$j];
+            }
+        }
+    }
+
+    for($i=0;$i<3;$i++){
+        for($j=0;$j<3;$j++){
+            echo $ans[$i][$j] . " ";
+        }
+        echo "<br>";
+    }
+?>
